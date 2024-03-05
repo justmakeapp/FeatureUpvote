d8980c3a851daa7674d74719d89c85e151afd694
Long Vu <long.vudai01@gmail.com>
2024-03-05 10:11:15 +0700

    set platforms from ios 16

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.9
 
 import PackageDescription
 
 let package = Package(
     name: "FeatureUpvote",
     defaultLocalization: "en",
-    platforms: [.iOS(.v15), .macOS(.v12)],
+    platforms: [.iOS(.v16), .macOS(.v13)],
     products: [
         .library(
             name: "FeatureUpvoteKit",
