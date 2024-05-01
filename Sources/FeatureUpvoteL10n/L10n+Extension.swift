76c6091acf3bae995293e37691849376a0fa5244
Long Vu <long.vudai01@gmail.com>
2024-05-01 13:59:23 +0700

    add bundle

--- Sources/FeatureUpvoteL10n/L10n+Extension.swift
+++ Sources/FeatureUpvoteL10n/L10n+Extension.swift
@@ -5,18 +5,5 @@
 import Foundation
 
 public extension L10n {
-    static func localizedString(
-        _ key: String,
-        tableName: String? = nil,
-        value: String = "",
-        comment: String = ""
-    ) -> String {
-        return NSLocalizedString(
-            key,
-            tableName: tableName,
-            bundle: .module,
-            value: value,
-            comment: comment
-        )
-    }
+    static let bundle = Bundle.module
 }
