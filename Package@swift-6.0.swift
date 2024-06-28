e8c0e7ea5b84b4bc744b8123fb780ce395229ece
Long Vu <long.vudai01@gmail.com>
2024-06-28 10:14:16 +0700

    add package swift 6

---
+++ Package@swift-6.0.swift
@@ -0,0 +1,39 @@
+// swift-tools-version:6.0
+
+import PackageDescription
+
+let package = Package(
+    name: "FeatureUpvote",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v16), .macOS(.v13)],
+    products: [
+        .library(
+            name: "FeatureUpvoteKit",
+            targets: ["FeatureUpvoteKit"]
+        ),
+        .library(
+            name: "FeatureUpvoteL10n",
+            targets: ["FeatureUpvoteL10n"]
+        ),
+        .library(
+            name: "FeatureUpvoteKitUI",
+            targets: ["FeatureUpvoteKitUI"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "FeatureUpvoteKit"
+        ),
+        .target(
+            name: "FeatureUpvoteL10n"
+        ),
+        .target(
+            name: "FeatureUpvoteKitUI",
+            dependencies: [
+                "FeatureUpvoteKit",
+                "FeatureUpvoteL10n"
+            ]
+        ),
+    ],
+    swiftLanguageVersions: [.v6]
+)
