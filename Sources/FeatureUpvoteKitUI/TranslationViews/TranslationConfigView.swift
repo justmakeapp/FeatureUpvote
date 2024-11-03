35038074f6086c93dc32e4677dc2d9847453b00d
Long Vu <long.vudai01@gmail.com>
2024-11-03 21:37:22 +0700

    refactor TranslationConfigView for improved readability and structure

--- Sources/FeatureUpvoteKitUI/TranslationViews/TranslationConfigView.swift
+++ Sources/FeatureUpvoteKitUI/TranslationViews/TranslationConfigView.swift
@@ -11,176 +11,178 @@ import SwiftUI
 #endif
 import FeatureUpvoteL10n
 
-@available(iOS 18.0, macOS 15.0, *)
-public struct TranslationConfigView: View {
-    public enum LanguageType {
-        case languageCode(String)
-        case localLanguage(Locale.Language)
-    }
+#if canImport(Translation) && (os(macOS) || (os(iOS) && !targetEnvironment(macCatalyst)))
+    @available(iOS 18.0, macOS 15.0, *)
+    public struct TranslationConfigView: View {
+        public enum LanguageType {
+            case languageCode(String)
+            case localLanguage(Locale.Language)
+        }
 
-    private let sourceLanguageType: LanguageType?
-    @State private var sourceLanguage: Locale.Language?
-    @Binding private var targetLanguage: Locale.Language?
-    @State private var availableLanguages = [Locale.Language]()
+        private let sourceLanguageType: LanguageType?
+        @State private var sourceLanguage: Locale.Language?
+        @Binding private var targetLanguage: Locale.Language?
+        @State private var availableLanguages = [Locale.Language]()
 
-    @Binding var configuration: TranslationSession.Configuration?
+        @Binding var configuration: TranslationSession.Configuration?
 
-    @Environment(\.dismiss) private var dismiss
+        @Environment(\.dismiss) private var dismiss
 
-    public init(
-        sourceLanguageType: LanguageType?,
-        targetLanguage: Binding<Locale.Language?>,
-        configuration: Binding<TranslationSession.Configuration?>
-    ) {
-        self.sourceLanguageType = sourceLanguageType
-        _sourceLanguage = .init(wrappedValue: {
-            guard let sourceLanguageType else {
-                return nil
-            }
-
-            switch sourceLanguageType {
-            case .languageCode:
-                return nil
-            case let .localLanguage(language):
-                return language
-            }
-        }())
-        _targetLanguage = targetLanguage
-        _configuration = configuration
-    }
-
-    public var body: some View {
-        containerView
-            .task { @MainActor in
-                let supportedLanguagesTask = Task.detached {
-                    return await LanguageAvailability().supportedLanguages
+        public init(
+            sourceLanguageType: LanguageType?,
+            targetLanguage: Binding<Locale.Language?>,
+            configuration: Binding<TranslationSession.Configuration?>
+        ) {
+            self.sourceLanguageType = sourceLanguageType
+            _sourceLanguage = .init(wrappedValue: {
+                guard let sourceLanguageType else {
+                    return nil
                 }
 
-                let result = await supportedLanguagesTask.value
+                switch sourceLanguageType {
+                case .languageCode:
+                    return nil
+                case let .localLanguage(language):
+                    return language
+                }
+            }())
+            _targetLanguage = targetLanguage
+            _configuration = configuration
+        }
 
-                if sourceLanguage == nil {
-                    if
-                        let sourceLanguageType,
-                        case let .languageCode(code) = sourceLanguageType {
-                        sourceLanguage = result.first(where: { $0.languageCode?.identifier == code })
-                    } else {
-                        sourceLanguage = result.first
+        public var body: some View {
+            containerView
+                .task { @MainActor in
+                    let supportedLanguagesTask = Task.detached {
+                        return await LanguageAvailability().supportedLanguages
                     }
+
+                    let result = await supportedLanguagesTask.value
+
+                    if sourceLanguage == nil {
+                        if
+                            let sourceLanguageType,
+                            case let .languageCode(code) = sourceLanguageType {
+                            sourceLanguage = result.first(where: { $0.languageCode?.identifier == code })
+                        } else {
+                            sourceLanguage = result.first
+                        }
+                    }
+
+                    targetLanguage = result
+                        .first(where: { $0.languageCode?.identifier == Locale.firstPreferredLanguageID })
+
+                    availableLanguages = result
                 }
+        }
 
-                targetLanguage = result
-                    .first(where: { $0.languageCode?.identifier == Locale.firstPreferredLanguageID })
+        @ViewBuilder
+        private var containerView: some View {
+            NavigationStack {
+                contentView
+                    .toolbar {
+                        #if os(macOS)
+                            ToolbarItem(placement: .cancellationAction) {
+                                cancelButton
+                            }
 
-                availableLanguages = result
+                            ToolbarItem(placement: .confirmationAction) {
+                                translateButton
+                            }
+                        #endif
+
+                        #if os(iOS)
+                            ToolbarItem(placement: .topBarTrailing) {
+                                cancelButton
+                            }
+                        #endif
+                    }
+                    .presentationDetents([.height(260)])
             }
-    }
+            .presentationDragIndicator(.visible)
+        }
 
-    @ViewBuilder
-    private var containerView: some View {
-        NavigationStack {
-            contentView
-                .toolbar {
-                    #if os(macOS)
-                        ToolbarItem(placement: .cancellationAction) {
-                            cancelButton
+        private var contentView: some View {
+            GeometryReader { proxy in
+                VStack(spacing: 24) {
+                    VStack {
+                        HStack {
+                            Label("Source Language", systemImage: "book.closed")
+
+                            Spacer()
+
+                            Picker("", selection: $sourceLanguage) {
+                                ForEach(availableLanguages, id: \.self) { language in
+                                    Text(Locale.current
+                                        .localizedString(forIdentifier: language.minimalIdentifier) ?? "")
+                                        .tag(language)
+                                }
+                            }
+                            .frame(minWidth: 100, maxWidth: max(100, proxy.size.width / 2))
                         }
 
-                        ToolbarItem(placement: .confirmationAction) {
-                            translateButton
+                        HStack {
+                            Label("Target Language", systemImage: "globe")
+
+                            Spacer()
+
+                            Picker("", selection: $targetLanguage) {
+                                ForEach(availableLanguages, id: \.self) { language in
+                                    Text(Locale.current
+                                        .localizedString(forIdentifier: language.minimalIdentifier) ?? "")
+                                        .tag(language)
+                                }
+                            }
+                            .frame(minWidth: 100, maxWidth: max(100, proxy.size.width / 2))
                         }
-                    #endif
+                    }
 
                     #if os(iOS)
-                        ToolbarItem(placement: .topBarTrailing) {
-                            cancelButton
-                        }
+                        translateButton
                     #endif
+
+                    Spacer()
                 }
-                .presentationDetents([.height(260)])
-        }
-        .presentationDragIndicator(.visible)
-    }
-
-    private var contentView: some View {
-        GeometryReader { proxy in
-            VStack(spacing: 24) {
-                VStack {
-                    HStack {
-                        Label("Source Language", systemImage: "book.closed")
-
-                        Spacer()
-
-                        Picker("", selection: $sourceLanguage) {
-                            ForEach(availableLanguages, id: \.self) { language in
-                                Text(Locale.current
-                                    .localizedString(forIdentifier: language.minimalIdentifier) ?? "")
-                                    .tag(language)
-                            }
-                        }
-                        .frame(minWidth: 100, maxWidth: max(100, proxy.size.width / 2))
-                    }
-
-                    HStack {
-                        Label("Target Language", systemImage: "globe")
-
-                        Spacer()
-
-                        Picker("", selection: $targetLanguage) {
-                            ForEach(availableLanguages, id: \.self) { language in
-                                Text(Locale.current
-                                    .localizedString(forIdentifier: language.minimalIdentifier) ?? "")
-                                    .tag(language)
-                            }
-                        }
-                        .frame(minWidth: 100, maxWidth: max(100, proxy.size.width / 2))
-                    }
-                }
-
-                #if os(iOS)
-                    translateButton
-                #endif
-
-                Spacer()
+                .padding()
             }
-            .padding()
-        }
-        #if os(macOS)
-        .frame(minHeight: 160)
-        #endif
-    }
-
-    private var translateButton: some View {
-        Button {
-            self.configuration = TranslationSession.Configuration(
-                source: sourceLanguage,
-                target: targetLanguage
-            )
-
-            dismiss()
-        } label: {
-            HStack {
-                Label(FeatureUpvoteL10n.L10n.Action.translate, systemImage: "translate")
-                    .foregroundStyle(.white)
-            }
-            #if os(iOS)
-            .frame(height: 44.scaledToMac())
-            .frame(maxWidth: 400)
-            .background(Color.accentColor)
-            .cornerRadius(8)
+            #if os(macOS)
+            .frame(minHeight: 160)
             #endif
         }
-        #if os(iOS)
-        .buttonStyle(PressEffectButtonStyle())
-        #endif
-        #if os(macOS)
-        .buttonStyle(.borderedProminent)
-        #endif
-        .disabled(sourceLanguage == nil && targetLanguage == nil)
-    }
 
-    private var cancelButton: some View {
-        Button(FeatureUpvoteL10n.L10n.Action.cancel) {
-            dismiss()
+        private var translateButton: some View {
+            Button {
+                self.configuration = TranslationSession.Configuration(
+                    source: sourceLanguage,
+                    target: targetLanguage
+                )
+
+                dismiss()
+            } label: {
+                HStack {
+                    Label(FeatureUpvoteL10n.L10n.Action.translate, systemImage: "translate")
+                        .foregroundStyle(.white)
+                }
+                #if os(iOS)
+                .frame(height: 44.scaledToMac())
+                .frame(maxWidth: 400)
+                .background(Color.accentColor)
+                .cornerRadius(8)
+                #endif
+            }
+            #if os(iOS)
+            .buttonStyle(PressEffectButtonStyle())
+            #endif
+            #if os(macOS)
+            .buttonStyle(.borderedProminent)
+            #endif
+            .disabled(sourceLanguage == nil && targetLanguage == nil)
+        }
+
+        private var cancelButton: some View {
+            Button(FeatureUpvoteL10n.L10n.Action.cancel) {
+                dismiss()
+            }
         }
     }
-}
+#endif
