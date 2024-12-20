ab21d30eb1fca335674087a98186c7dfcd4bd62f
Long Vu <long.vudai01@gmail.com>
2024-12-20 14:57:33 +0700

    update Swift tools version to 6.0 and remove deprecated package file

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version:6.0
 
 import PackageDescription
 
@@ -34,5 +34,6 @@ let package = Package(
                 "FeatureUpvoteL10n"
             ]
         ),
-    ]
+    ],
+    swiftLanguageModes: [.v6]
 )
