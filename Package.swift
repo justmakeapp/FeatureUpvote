0612ab8f9dce89886be943283157dcbf17d646c7
Long Vu <long.vudai01@gmail.com>
2025-09-04 23:03:52 +0700

    update platform versions to iOS 17 and macOS 14 in Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "FeatureUpvote",
     defaultLocalization: "en",
-    platforms: [.iOS(.v16), .macOS(.v13)],
+    platforms: [.iOS(.v17), .macOS(.v14)],
     products: [
         .library(
             name: "FeatureUpvoteKit",
